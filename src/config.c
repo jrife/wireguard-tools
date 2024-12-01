d7b29816d31acc0f5e35ec073c12d5454a4068dc
Jordan Rife <jrife0@gmail.com>
2024-11-30 20:19:35 -0800

    ipc: linux: Support incremental allowed ips updates
    [1] adds support for the WGALLOWEDIP_F_REMOVE_ME flag to WireGuard's
    Linux driver which is a direct way of removing a single allowed ip from
    a peer. Extend the interface of `wg set` to leverage this feature,
    allowing for incremental updates to a peer's configuration. By default,
    allowed-ips fully replaces a peer's allowed ips using
    WGPEER_REPLACE_ALLOWEDIPS under the hood. When '+' or '-' is prepended
    to any ip in the list, wg clears WGPEER_F_REPLACE_ALLOWEDIPS and sets
    the WGALLOWEDIP_F_REMOVE_ME flag on any ip prefixed with '-'.
    
    $ wg set wg0 peer <PUBKEY> allowed-ips +192.168.88.0/24,-192.168.0.1/32
    
    This command means "add 192.168.88.0/24 to this peer's allowed ips if
    not present, and remove 192.168.0.1/32 if present".
    
    TODO: '-' doesn't work on other systems.
    
    [1]: https://lore.kernel.org/netdev/20240905200551.4099064-1-jrife@google.com/
    
    Signed-off-by: Jordan Rife <jrife@google.com>

--- src/config.c
+++ src/config.c
@@ -337,6 +337,29 @@ static bool validate_netmask(struct wgallowedip *allowedip)
 	return true;
 }
 
+#if defined(__linux__)
+static inline void parse_ip_prefix(struct wgpeer *peer, uint32_t *flags, char **mask)
+{
+	/* If the IP is prefixed with either '+' or '-' consider
+	 * this an incremental change. Disable WGPEER_REPLACE_ALLOWEDIPS.
+	 */
+	switch ((*mask)[0]) {
+	case '-':
+		*flags |= WGALLOWEDIP_REMOVE_ME;
+		/* fall through */
+	case '+':
+		peer->flags &= ~WGPEER_REPLACE_ALLOWEDIPS;
+		(*mask)++;
+	}
+}
+#else
+static inline void parse_ip_prefix(struct wgpeer *peer __attribute__ ((unused)),
+				   uint32_t *flags     __attribute__ ((unused)),
+				   char **mask         __attribute__ ((unused)))
+{
+}
+#endif
+
 static inline bool parse_allowedips(struct wgpeer *peer, struct wgallowedip **last_allowedip, const char *value)
 {
 	struct wgallowedip *allowedip = *last_allowedip, *new_allowedip;
@@ -353,8 +376,11 @@ static inline bool parse_allowedips(struct wgpeer *peer, struct wgallowedip **la
 	}
 	sep = mutable;
 	while ((mask = strsep(&sep, ","))) {
+		uint32_t flags = 0;
 		unsigned long cidr;
 		char *end, *ip;
+
+		parse_ip_prefix(peer, &flags, &mask);
 
 		saved_entry = strdup(mask);
 		ip = strsep(&mask, "/");
@@ -387,6 +413,7 @@ static inline bool parse_allowedips(struct wgpeer *peer, struct wgallowedip **la
 		else
 			goto err;
 		new_allowedip->cidr = cidr;
+		new_allowedip->flags = flags;
 
 		if (!validate_netmask(new_allowedip))
 			fprintf(stderr, "Warning: AllowedIP has nonzero host part: %s/%s\n", ip, mask);
